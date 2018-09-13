fd9702b128cf7887f9edacd1a93c011bd6f313e7
wmcosta <wmcosta@latam.stefanini.com>
2018-09-13 13:01:31 -0300

    Configuração do eureka

--- src/main/java/com/stefanini/poceureka/PocEurekaApplication.java
+++ src/main/java/com/stefanini/poceureka/PocEurekaApplication.java
@@ -2,8 +2,10 @@ package com.stefanini.poceureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PocEurekaApplication {
 
 	public static void main(String[] args) {
