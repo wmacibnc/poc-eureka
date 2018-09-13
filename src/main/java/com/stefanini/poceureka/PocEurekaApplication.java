9e8ea660875608fab16983d25d43317453ce28b2
wmcosta <wmcosta@latam.stefanini.com>
2018-09-13 09:20:02 -0300

    Adicionando dependencias iniciais

---
+++ src/main/java/com/stefanini/poceureka/PocEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.stefanini.poceureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocEurekaApplication.class, args);
+	}
+}
